bbee089576e29712d5c7b0c59bfee7dd9185b320
pabloferr92 <pabloferr92@gmail.com>
2020-10-14 22:30:11 -0300

    commit

--- test/test_pendenciamodel.py
+++ test/test_pendenciamodel.py
@@ -1,13 +1,19 @@
 from domain.model.Pendencia import Pendencia
+import pytest
 
 def test_pendencia():
     #arrange
-    nome = "PendenciaEsperada"
-    status = "pendente"
-    # act
-    pend = Pendencia(nome, status)
-    # assert 
-    assert pend.name == nome
-    assert pend.status == status
-    assert type(pend.name) == str
+    # nome = "PendenciaEsperada"
+    # status = "pendente"
+    # # act
+    # pend = Pendencia(nome, status)
+    # # assert 
+    # assert pend.name == nome
+    # assert pend.status == status
+    # assert type(pend.name) == str
+
+    with pytest.raises(DadosMinimosException):
+        pend = Pendencia()
+        if not pent.name and not pend.status:
+            raise Excpetion()
     
